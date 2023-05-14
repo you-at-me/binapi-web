e1ca9a4456f8ea5da5a695c24433da9409b51b5c
zyshu <zyshu11571@outlook.com>
2023-05-14 21:29:43 +0800

    add user auth

--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,13 @@
 /**
- * @see https://umijs.org/zh-CN/plugins/plugin-access
- * */
-export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
+ * 权限校验
+ * @see https://umijs.org/docs/max/access
+ */
+export default function access(initialState: InitialState | undefined) {
+  const { loginUser } = initialState ?? {};
+
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    canUser: loginUser, // 普通用户权限
+    canAdmin: loginUser?.userRole == "admin" // 现改成我们设置的配置，只有当我们的用户角色是 admin 才具备管理员权限; 用户角色是在数据库当中配置的
+    // canAdmin: currentUser && currentUser.access == "admin", // 管理员权限，并且得是 admin 用户才行; 这是默认的校验配置
   };
 }
